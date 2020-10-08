078dc0fbc13b52dc45315be242ca0e0cd60d43c5
Mario Celada <mcelam00@estudiantes.unileon.es>
2020-10-08 19:00:51 +0200

    Esqueleto de la factoria concreta ConcretaFactoryIngles creada

---
+++ ISO_patternsTODO/src/ConcretaFactoryIngles.java
@@ -0,0 +1,21 @@
+
+public class ConcretaFactoryIngles implements AbstractFactoryIdiomas{
+	/**
+	 * Factoría concreta que va a implementar todos los metodos de la interfaz factoria
+	 * 
+	 */
+	public ConcretaFactoryIngles() {
+		
+	}
+	
+	public Moneda getMoneda() {
+		
+	}
+	
+	public Lista getLista() {
+		
+	}
+	
+	
+
+}
