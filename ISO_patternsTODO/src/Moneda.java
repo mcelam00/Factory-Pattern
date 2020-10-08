4eca7dd97b36c8841591453374e03946617ea902
Mario Celada <mcelam00@estudiantes.unileon.es>
2020-10-08 22:55:55 +0200

    Completada la clase Moneda

--- ISO_patternsTODO/src/Moneda.java
+++ ISO_patternsTODO/src/Moneda.java
@@ -7,35 +7,67 @@ public abstract class Moneda {
 	 *    MonedaUSA
 	 */
 	
-	private String texto;
-	private double valor;
+	private String texto; //atributo texto para la moneda
+	private double valor; //Atributo valor para la moneda
 	
+	/**
+	 * Constructor de la clase: No se puede instanciar una clase abstracta sería para usar desde las subclases
+	 */
+		
 	public Moneda() {
 		
 	}
 	
+	/**
+	 * Getter
+	 * @return el atributo texto
+	 */
 	
 	public String getTexto() {
-		
+		return this.texto;
 	}
+	
+	/**
+	 * Setter
+	 * @param texto El valor de tecto que queramos ponerle a la moneda
+	 */
 	
 	public void setTexto(String texto) {
-		
+		this.texto = texto;
 	}
 	
+	/**
+	 * Getter
+	 * @return el atributo valor
+	 */
 	
 	public double getValor() {
-		
+		return this.valor;
 	}
 	
+	/**
+	 * Setter
+	 * @param valor El valor numerico en coma flotante de precisión doble que queramos ponerle a la moneda
+	 */
 	
 	public void setValor(double valor) {
-		
+		this.valor = valor;
 	}
 	
+	/**
+	 * Método que imprime la moneda creada
+	 * 
+	 */
+	
 	public void visualizar() {
-		
+		System.out.println("Moneda texto: "+this.texto+"\n"+"Moneda valor: "+this.valor+"\n"); //heredan el getter ambos concretos
+
 	}
+	
+	/**
+	 * Método abstracto implementado en las subclases
+	 */
+	
 	
 	public abstract void configurar();	
 	
