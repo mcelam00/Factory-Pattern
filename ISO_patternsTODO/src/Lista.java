4223ff1129277fd4f04b63cd5d3debeb892d82ba
Mario Celada <mcelam00@estudiantes.unileon.es>
2020-10-08 18:22:16 +0200

    Esqueleto de la clase Lista creado

---
+++ ISO_patternsTODO/src/Lista.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public abstract class Lista {
+	/**
+	 * Clase que incluye metodos implementados y sin implementar 
+	 * El método configurar será abstracto e implementado por las clases que heredan de esta clase
+	 *    ListaEspañol
+	 *    ListaIngles
+	 */
+	
+	
+	private ArrayList<Integer> elementos;
+	
+	
+	public Lista() {
+		
+	}
+	
+	
+	public int getElemento(int indice) {
+		
+	}
+	
+	
+	public void insertarElemento(int elem) {
+		
+	}
+	
+	
+	public void visualizar() {
+		
+	}
+	
+	abstract public void configurar();
+	
+	
+	
+	
+
+}
