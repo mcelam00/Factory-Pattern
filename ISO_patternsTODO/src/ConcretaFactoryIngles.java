da0aaae46eb0f4e45c77f08933bfe8b7a99f844b
Mario Celada <mcelam00@estudiantes.unileon.es>
2020-10-08 22:34:26 +0200

    Completada la clase ConcretaFactoryIngles

--- ISO_patternsTODO/src/ConcretaFactoryIngles.java
+++ ISO_patternsTODO/src/ConcretaFactoryIngles.java
@@ -4,18 +4,40 @@ public class ConcretaFactoryIngles implements AbstractFactoryIdiomas{
 	 * Factoría concreta que va a implementar todos los metodos de la interfaz factoria
 	 * 
 	 */
+	
 	public ConcretaFactoryIngles() {
 		
 	}
 	
+	/**
+	 * Esta factoría concreta se va a encargar de llamar ella al constructor de la moneda USA
+	 * dólar estadounidense equivale a 0,85 euro
+	 */
+	
 	public Moneda getMoneda() {
+		Moneda monConcretaUSA;
+		
+		monConcretaUSA = new MonedaUSA(0.85);
+		monConcretaUSA.configurar();
+		
+		return monConcretaUSA;
 		
 	}
+	
+	/**
+	 * Esta factoría concreta se va a encargar de llamar ella al constructor de la lista Ingles
+	 */
 	
 	public Lista getLista() {
+		Lista listConcretaIng; //creo referencia de tipo la superclase
 		
-	}
-	
+		listConcretaIng = new ListaIngles(); //creo el objeto concreto de manera que referencio a él con mi referencia superclase
+		listConcretaIng.configurar();
+		
+		
+		return listConcretaIng; //lo devuelvo
+		
+	}	
 	
 
 }
