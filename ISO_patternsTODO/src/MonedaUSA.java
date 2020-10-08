2dd16ff23d75cf257a417ce44d6deae197930879
Mario Celada <mcelam00@estudiantes.unileon.es>
2020-10-08 22:35:04 +0200

    Completada la clase MonedaUSA

--- ISO_patternsTODO/src/MonedaUSA.java
+++ ISO_patternsTODO/src/MonedaUSA.java
@@ -1,12 +1,23 @@
 
 public class MonedaUSA extends Moneda{
 	
+	/**
+	 * Al llamar al constructor se asigna el valor
+	 */
+	
 	public MonedaUSA(double valor) {
-		
+		this.setValor(valor);
 	}
+	
+	/**
+	 * Al llamar a configurar se estipula su tipo de moneda
+	 */
+	
 	
 	public void configurar() {
 		
+		this.setTexto("Dólar Estadounidense");
+
 	}
 	
 
