7e0d44c6e8f38ec209a6a21c5ec9347798580ca6
Mario Celada <mcelam00@estudiantes.unileon.es>
2020-10-08 22:11:16 +0200

    Completada la clase ListaEspañol

--- ISO_patternsTODO/src/ListaEspañol.java
+++ ISO_patternsTODO/src/ListaEspañol.java
@@ -2,14 +2,22 @@ import java.awt.List;
 
 public class ListaEspañol extends Lista{
 	
+	/**
+	 * Crea una instancia de la listaEspañol
+	 */
+	
 	public ListaEspañol() {
 		
 	}
 
+	/**
+	 * Inserta un valor en la lista 
+	 */
+	
 	
 	public void configurar() {
 
-
+		this.insertarElemento(1);
 		
 	}
 	
