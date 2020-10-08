8493ccc513ebf570ff67d74c32062a9ebbe53822
Mario Celada <mcelam00@estudiantes.unileon.es>
2020-10-08 19:04:22 +0200

    Esqueleto de la factoría concreta ConcretaFactoryEspañol creado

---
+++ ISO_patternsTODO/src/ConcretaFactoryEspañol.java
@@ -0,0 +1,24 @@
+
+public class ConcretaFactoryEspañol implements AbstractFactoryIdiomas{
+	/**
+	 * Factoría concreta que va a implementar todos los metodos de la interfaz factoria
+	 * 
+	 */
+	
+	public ConcretaFactoryEspañol() {
+		
+	}
+	
+	public Moneda getMoneda() {
+		
+	}
+	
+	
+	public Lista getLista() {
+		
+	}	
+	
+	
+	
+	
+}
