7c8b792e3f90c8a6bdd42e701daf4a1af5d7cd30
Mario Celada <mcelam00@estudiantes.unileon.es>
2020-10-08 22:34:50 +0200

    Completada la clase ListaIngles

--- ISO_patternsTODO/src/ListaIngles.java
+++ ISO_patternsTODO/src/ListaIngles.java
@@ -1,14 +1,21 @@
 
 public class ListaIngles extends Lista{
 
+	/**
+	 * Crea una instancia de la listaIngles
+	 */
+	
 	public ListaIngles() {
 		
 	}
 	
+	/**
+	 * Inserta una cantidad de monedas en la lista 
+	 */
 	
 	public void configurar() {
 		
-		
+		this.insertarElemento(1);
 		
 		
 	}	
